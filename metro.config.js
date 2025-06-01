24da05e96f1dceb9e6c8ed35c3c616d7bf929866
David Martins <contato@davidmartins.net>
2025-06-01 16:02:59 -0300

    replace local build with eas build

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// learn more https://docs.expo.io/guides/customizing-metro
+const { getdefaultconfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').metroconfig} */
+const config = getdefaultconfig(__dirname);
 
 module.exports = config;
