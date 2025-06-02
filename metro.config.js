8735d105c53fba6bdd6c78e9168b286de2bebe96
David Martins <contato@davidmartins.net>
2025-06-02 08:37:21 -0300

    fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 // learn more https://docs.expo.io/guides/customizing-metro
-const { getdefaultconfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').metroconfig} */
-const config = getdefaultconfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
